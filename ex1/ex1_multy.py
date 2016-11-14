dcbeac5836127031a52ea9c784697313cbccaed6
Calahown <blind.noldor@gmail.com>
2016-11-14 04:43:54 -0600

    ex1 second part

---
+++ ex1/ex1_multy.py
@@ -0,0 +1,14 @@
+from matplotlib import use
+use('TkAgg')
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+	print("Loading data...")
+	data = np.loadtxt("ex1data2.txt", delimiter = ',')
+	
+
+
+
+
+main()
